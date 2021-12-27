191dccc3e5caa21acbe777fd9ca2a85d5775fe7e
Oscar Rosner <oscar.rosner@web.de>
2021-12-27 03:49:53 +0100

    setup jest test runner

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,10 @@
+describe('a', () => {
+  it('does something', () => {
+    /* === Arrange =========== */
+
+    /* === Act =============== */
+
+    /* === Assert ============ */
+    expect(true).toBe(true);
+  });
+});
