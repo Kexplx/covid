050e3a5e735ee4a77fd74191fe55c33d01e86730
Oscar Rosner <oscar.rosner@web.de>
2022-01-03 00:41:38 +0100

    cleanup docs

--- frontend/src/app/environment-provider.ts
+++ frontend/src/app/environment-provider.ts
@@ -1,12 +1,12 @@
 import { InjectionToken, Provider } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-// For components and services to use as
-// the type of the environment config.
-export type Environment = typeof environment;
-
 // For components and services to inject.
 export const ENVIRONMENT_TOKEN = new InjectionToken<Environment>('Environment injection token');
+
+// For components and services to use as
+// the type of the injected environment object.
+export type Environment = typeof environment;
 
 // For AppModule to provide.
 export const environmentProvider: Provider = {
