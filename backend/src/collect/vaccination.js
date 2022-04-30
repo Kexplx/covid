d82f2c1c8739654041e71676c6d8ade5a51b3706
Oscar Rosner <oscar.rosner@web.de>
2022-04-30 17:01:48 +0200

    fix vaccination cols

--- backend/src/collect/vaccination.js
+++ backend/src/collect/vaccination.js
@@ -17,10 +17,10 @@ async function getVaccination() {
   return {
     lastUpdated: parseGermanStringDateToISOString(/(\d+.\d+.\d+, \d+:\d+ Uhr)/.exec(sheet2['A3'].v)[1]),
     numberOfPeopleAtLeastOnceVaccinated: cell_sheet1('D21'),
-    percentAtLeastOnceVaccinated: cell_sheet1('G21'),
+    percentAtLeastOnceVaccinated: cell_sheet1('H21'),
     totalNumberOfVaccinations: cell_sheet1('C21'),
     bavaria_numberOfPeopleAtLeastOnceVaccinated: cell_sheet1('D5'),
-    bavaria_percentAtLeastOnceVaccinated: cell_sheet1('G5'),
+    bavaria_percentAtLeastOnceVaccinated: cell_sheet1('H5'),
     stateWithLowestVaccination: states[0],
     stateWithHighestVaccination: states[states.length - 1],
   };
@@ -28,22 +28,22 @@ async function getVaccination() {
 
 function getStatesAscendingByFirstTimeVaccination(getCell) {
   const states = [
-    { name: 'Baden-Württemberg', percentAtLeastOnceVaccinated: getCell('G4') },
-    { name: 'Bayern', percentAtLeastOnceVaccinated: getCell('G5') },
-    { name: 'Berlin', percentAtLeastOnceVaccinated: getCell('G6') },
-    { name: 'Brandenburg', percentAtLeastOnceVaccinated: getCell('G7') },
-    { name: 'Bremen', percentAtLeastOnceVaccinated: getCell('G8') },
-    { name: 'Hamburg', percentAtLeastOnceVaccinated: getCell('G9') },
-    { name: 'Hessen', percentAtLeastOnceVaccinated: getCell('G10') },
-    { name: 'Mecklenburg-Vorpommern', percentAtLeastOnceVaccinated: getCell('G11') },
-    { name: 'Niedersachsen', percentAtLeastOnceVaccinated: getCell('G12') },
-    { name: 'Nordrhein-Westfalen', percentAtLeastOnceVaccinated: getCell('G13') },
-    { name: 'Rheinland-Pfalz', percentAtLeastOnceVaccinated: getCell('G14') },
-    { name: 'Saarland', percentAtLeastOnceVaccinated: getCell('G15') },
-    { name: 'Sachsen', percentAtLeastOnceVaccinated: getCell('G16') },
-    { name: 'Sachsen-Anhalt', percentAtLeastOnceVaccinated: getCell('G17') },
-    { name: 'Schleswig-Holstein', percentAtLeastOnceVaccinated: getCell('G18') },
-    { name: 'Thüringen', percentAtLeastOnceVaccinated: getCell('G19') },
+    { name: 'Baden-Württemberg', percentAtLeastOnceVaccinated: getCell('H4') },
+    { name: 'Bayern', percentAtLeastOnceVaccinated: getCell('H5') },
+    { name: 'Berlin', percentAtLeastOnceVaccinated: getCell('H6') },
+    { name: 'Brandenburg', percentAtLeastOnceVaccinated: getCell('H7') },
+    { name: 'Bremen', percentAtLeastOnceVaccinated: getCell('H8') },
+    { name: 'Hamburg', percentAtLeastOnceVaccinated: getCell('H9') },
+    { name: 'Hessen', percentAtLeastOnceVaccinated: getCell('H10') },
+    { name: 'Mecklenburg-Vorpommern', percentAtLeastOnceVaccinated: getCell('H11') },
+    { name: 'Niedersachsen', percentAtLeastOnceVaccinated: getCell('H12') },
+    { name: 'Nordrhein-Westfalen', percentAtLeastOnceVaccinated: getCell('H13') },
+    { name: 'Rheinland-Pfalz', percentAtLeastOnceVaccinated: getCell('H14') },
+    { name: 'Saarland', percentAtLeastOnceVaccinated: getCell('H15') },
+    { name: 'Sachsen', percentAtLeastOnceVaccinated: getCell('H16') },
+    { name: 'Sachsen-Anhalt', percentAtLeastOnceVaccinated: getCell('H17') },
+    { name: 'Schleswig-Holstein', percentAtLeastOnceVaccinated: getCell('H18') },
+    { name: 'Thüringen', percentAtLeastOnceVaccinated: getCell('H19') },
   ].sort((a, b) => a.percentAtLeastOnceVaccinated - b.percentAtLeastOnceVaccinated); // Sort ascending.
 
   return states;
